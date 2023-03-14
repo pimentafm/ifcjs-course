2867e4e4335aea690e90fd9e46b85bb5d46910f1
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2023-03-14 18:45:59 -0300

    Add firebase to project

--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,24 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { App } from "./App";
 
+import { initializeApp } from "firebase/app";
+
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCTBdagIhNnW8iZOEuWWauO37AYG7z6Yzw",
+  authDomain: "fir-ifcjs-course-d11f0.firebaseapp.com",
+  projectId: "fir-ifcjs-course-d11f0",
+  storageBucket: "fir-ifcjs-course-d11f0.appspot.com",
+  messagingSenderId: "115803997000",
+  appId: "1:115803997000:web:71fd70aed13fd97337e97d",
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
 
 root.render(
   <React.StrictMode>
