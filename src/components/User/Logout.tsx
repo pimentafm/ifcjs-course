8ef4c8259d80839549d63b6b8b04b0213194cfb3
Fernando Pimenta <fernando.m.pimenta@gmail.com>
2023-03-14 21:53:28 -0300

    Add logout

---
+++ src/components/User/Logout.tsx
@@ -0,0 +1,5 @@
+import { FC } from "react";
+
+export const Logout: FC = () => {
+  return <button className="coolButton">Logout</button>;
+};
--- src/components/User/User.tsx
+++ src/components/User/User.tsx
@@ -1,8 +1,13 @@
 import { FC } from "react";
 
 import { useUserContext } from "../../UserProvider";
+import { Logout } from "./Logout";
 
 export const User: FC = () => {
   const [user] = useUserContext();
-  return <>{user?.displayName}</>;
+  return (
+    <div className="contentFlexVertical">
+      {user?.displayName} <Logout />
+    </div>
+  );
 };
